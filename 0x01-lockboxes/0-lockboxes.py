d8aef88603629c52227c4b46718cb5fa0cc9c924
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-08-05 07:46:44 +0000

    pep8 fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,24 +2,24 @@
 """solve lockboxes"""
 
 
-def join(T,R):
-    res =[]
-    for e in R:
-      res += T[e]
+def join(T, R):
+    res = []
+    for i in R:
+        res += T[i]
     return res
+
 
 def canUnlockAll(boxes):
     """determines if all the boxes can be opened."""
     index = 0
-    total = list(set(boxes[0])| {0})
+    total = list(set(boxes[0]) | {0})
     added = True
     while added:
-      added = False
-      for j in join(boxes,total[index:]):
-        if j not in total:
-          total.append(j)
-          index +=1
-          added= True
-#   print(total)
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index += 1
+                added = True
 
-    return len(total)==len(boxes)
+    return len(total) == len(boxes)
